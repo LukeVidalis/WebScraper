fc2b92111d57972ba1bef901691c4c761bd89fc1
guillech <gchibas@mac.com>
2019-02-11 14:33:59 +0000

    Page loop
    rowList is now a field in the class and split saveData into two
    methods. saveData() now only passes the information from rowList to the
    .csv. saveRow add the new TableRows to rowList.

--- infoRetriever.java
+++ infoRetriever.java
@@ -13,35 +13,39 @@ import org.jsoup.select.Elements;
 // Test
 public class infoRetriever {
 	final static String url = "http://www.betfellas.gr/tipster/SerSira/?period=2018&page=";
+	private ArrayList<TableRow> rowsList = new ArrayList<TableRow>();
 
 	public infoRetriever() throws IOException {
 		getSite();
 	}
 
 	private void getSite() throws IOException {
-		Document doc = Jsoup.connect(url + "1").get();
+		for(int i=0;i<20;i++) {
+			Document doc = Jsoup.connect(url + i).get();
+			Elements tables = doc.getElementsByClass("table table-striped");
+			Element tipTable = tables.last();
+			Elements rows = tipTable.getElementsByTag("tr");
 
-		Elements tables = doc.getElementsByClass("table table-striped");
-		Element tipTable = tables.last();
-		Elements rows = tipTable.getElementsByTag("tr");
-
-		saveData(rows);
+			saveRow(rows);
+		}
+		saveData();
 
 	}
-
-	private void saveData(Elements rows) throws IOException {
-		ArrayList<TableRow> rowsList = new ArrayList<TableRow>();
-
+	
+	private void saveRow(Elements rows) {
 		for (int i = 1; i < rows.size(); i++) {
 			TableRow tr = new TableRow(rows.get(i));
 			rowsList.add(tr);
-
-
 		}
+	}
+
+	private void saveData() throws IOException {
 		DataSaver ds = new DataSaver();
 		ds.addData(rowsList);
 		output(rowsList);
 	}
+	
+	
 
 	private void output(ArrayList<TableRow> rowsList) {
 		for (TableRow r : rowsList) {
